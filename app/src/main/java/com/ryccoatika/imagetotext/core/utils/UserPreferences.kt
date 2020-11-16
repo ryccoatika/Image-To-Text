fd9e171b6415484f30c45074886bfa9a1c16cc5a
ryccoatika <ryccoatika.ra@gmail.com>
2020-11-17 04:39:42 +0700

    fallback to previous alpha version of jetpack datastore because of there are some bug on it

--- app/src/main/java/com/ryccoatika/imagetotext/core/utils/UserPreferences.kt
+++ app/src/main/java/com/ryccoatika/imagetotext/core/utils/UserPreferences.kt
@@ -1,11 +1,11 @@
 package com.ryccoatika.imagetotext.core.utils
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
+import androidx.datastore.DataStore
+import androidx.datastore.preferences.Preferences
+import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.preferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
