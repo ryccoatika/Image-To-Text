0309b6d7037cfc3711d88085ccf6a11ecfec9020
rycco <ryccoatika@gmail.com>
2023-01-09 12:00:47 +0700

    initiate App Design System

---
+++ ui/src/main/java/com/ryccoatika/imagetotext/ui/common/Theme.kt
@@ -0,0 +1,33 @@
+package com.ryccoatika.imagetotext.ui.common
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+
+private val AppDarkColorScheme = darkColorScheme()
+
+private val AppLightColorScheme = lightColorScheme()
+
+private val AppShape = Shapes()
+
+private val AppType = Typography()
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        AppDarkColorScheme
+    } else {
+        AppLightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        shapes = AppShape,
+        typography = AppType,
+        content = content
+    )
+}
